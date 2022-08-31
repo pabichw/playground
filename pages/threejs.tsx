be2c83838666262fd065a3cd440417ec180eb25f
Wiktor Pabich <pabichwiktor@gmail.com>
2022-08-31 20:11:43 +0200

    feat(threejs): add orbit controller

--- pages/threejs.tsx
+++ pages/threejs.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import { useRef, useEffect } from 'react';
 import * as THREE from 'three'
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
 
 import styles from '../styles/Threejs.module.css'
 
 let scene: THREE.Scene; 
-let camera: THREE.Camera;
+let camera: THREE.PerspectiveCamera;
 let renderer: THREE.Renderer;
+let controls: any; // no type for this
 
 const ThreejsPage: NextPage = () => {
   const root = useRef<HTMLDivElement>(null);
@@ -15,7 +17,12 @@ const ThreejsPage: NextPage = () => {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
     renderer = new THREE.WebGLRenderer();
-    
+
+    controls = new TrackballControls(camera, renderer.domElement);    
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+
     renderer.setSize( window.innerWidth, window.innerHeight );
     
     root?.current?.appendChild( renderer.domElement );
@@ -23,14 +30,14 @@ const ThreejsPage: NextPage = () => {
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     const cube = new THREE.Mesh( geometry, material );
-    
+     
     scene.add( cube );
     camera.position.z = 5;
     
     const animate = function () {
       requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+
+      controls.update();
       renderer?.render( scene, camera );
     };
     
